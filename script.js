123b967a7997952c72eab24422144f12729b4370
Safin133847 <157776203+Safin133847@users.noreply.github.com>
2024-10-29 19:28:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+    input.addEventListener('input', (e) => {
+        // Move to the next input on entering a number
+        if (input.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+        // Move back to the previous input on backspace
+        if (e.key === 'Backspace' && index > 0 && input.value.length === 0) {
+            inputs[index - 1].focus();
+        }
+    });
+});
